88ffee3c8179bd727a74a7b94963489cc098c7e9
Tom <tom0917952503@gmail.com>
2023-03-16 18:39:19 +0800

    將reducer改為redux toolkit的寫法

--- store/account.js
+++ store/account.js
@@ -1,51 +1,90 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 // const ADD = "add";
 // const DELETE = "delete";
 // const EDIT = "edit";
-
-export default function reducer(state = []/*預設為空*/, action){
-    switch(action.type){
-
-        case accountAdded.type:
-            let newKey = 0;
-            for(i = 0; i < 999999; i++)
-            {
-                const index = state.findIndex(function(element) {
-                    return element.key == i
-                });
-                if(index == -1){
-                    newKey = i;
-                    break;
-                }
-            }
-            return [...state,
-                    {
-                        key: newKey, 
-                        name: action.payload.name, 
-                        value: action.payload.value,
-                        type: action.payload.type}]     
-
-        case accountDeleted.type:
-            return state.filter(item => item.key!==action.payload.key);
-
-
-        case accountEdited.type:
-            
-            return state.map(item => item.key===action.payload.key ? 
-                {...item, name: action.payload.name, value: action.payload.value, type: action.payload.type} :
-                 item);
-
-        default:
-            return state;
-    }
-}
-
-
-
-export const accountAdded = createAction("add");
+export const accountAdded = createAction("add"); //"add"為accountAdded的type
 export const accountDeleted = createAction("delete")
 export const accountEdited = createAction("edit")
+
+//https://redux-toolkit.js.org/api/createReducer
+
+const reducer = createReducer([], (builder) => {
+    builder
+      .addCase(accountAdded, (state, action) => {
+        let newKey = 0;
+        for(i = 0; i < 999999; i++)
+        {
+            const index = state.findIndex(function(element) {
+                return element.key == i
+            });
+            if(index == -1){
+                newKey = i;
+                break;
+            }
+        }
+        //如果有mutate state，就不用return
+        state.push({
+            key: newKey, 
+            name: action.payload.name, 
+            value: action.payload.value,
+            type: action.payload.type});
+      })
+      .addCase(accountDeleted, (state, action) => {
+        //沒有mutate state，要加return
+        return state.filter(item => item.key!==action.payload.key);
+      })
+      .addCase(accountEdited, (state, action) => {
+        return state.map(item => item.key===action.payload.key ? 
+            {...item, name: action.payload.name, value: action.payload.value, type: action.payload.type} :
+             item);
+      })
+
+}
+
+)
+export default reducer;
+
+// export default function reducer(state = []/*預設為空*/, action){
+//     switch(action.type){
+
+//         case accountAdded.type:
+//             let newKey = 0;
+//             for(i = 0; i < 999999; i++)
+//             {
+//                 const index = state.findIndex(function(element) {
+//                     return element.key == i
+//                 });
+//                 if(index == -1){
+//                     newKey = i;
+//                     break;
+//                 }
+//             }
+//             return [...state,
+//                     {
+//                         key: newKey, 
+//                         name: action.payload.name, 
+//                         value: action.payload.value,
+//                         type: action.payload.type}]     
+
+//         case accountDeleted.type:
+//             return state.filter(item => item.key!==action.payload.key);
+
+
+//         case accountEdited.type:
+            
+//             return state.map(item => item.key===action.payload.key ? 
+//                 {...item, name: action.payload.name, value: action.payload.value, type: action.payload.type} :
+//                  item);
+
+//         default:
+//             return state;
+//     }
+// }
+
+
+
+
 
 // export function accountAdded(inputText, inputNumber, inputType){
 //     return {
